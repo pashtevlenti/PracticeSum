9dda55a3034cbd79726249e5ee89484ef23c9ebd
pashtevlenti <pasha.solodyankin.06@mail.ru>
2024-07-01 03:15:02 +0300

    lala

---
+++ app/src/main/java/ru/itis/summerpractice/TypeDrive.kt
@@ -0,0 +1,5 @@
+package ru.itis.summerpractice
+
+enum class TypeDrive {
+    ALL,FRONT,BACK
+}
---
+++ app/src/main/java/ru/itis/summerpractice/TypeMotor.kt
@@ -0,0 +1,5 @@
+package ru.itis.summerpractice
+
+enum class TypeMotor {
+    ELECTRIC,DISEL,GASOLINE
+}
